e337768e1625479f8d4a91584aa56b7ab783f01b
Nathan <35500370+nathan-miller23@users.noreply.github.com>
2020-11-18 02:44:25 -0600

    remove dolphinwx dependency

--- Source/Core/Core/HW/EXI_DeviceSlippi.cpp
+++ Source/Core/Core/HW/EXI_DeviceSlippi.cpp
@@ -29,8 +29,8 @@
 #include "Core/State.h"
 
 // Not clean but idk a better way atm
-#include "DolphinWX/Frame.h"
-#include "DolphinWX/Main.h"
+//#include "DolphinWX/Frame.h"
+//#include "DolphinWX/Main.h"
 
 #define FRAME_INTERVAL 900
 #define SLEEP_TIME_MS 8
